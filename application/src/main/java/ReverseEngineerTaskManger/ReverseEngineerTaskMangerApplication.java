7ca86399b535f2b92aded33cccdfb568751b6da0
Yaseen asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2022-08-20 16:04:40 +0300

    Initial structure

---
+++ application/src/main/java/ReverseEngineerTaskManger/ReverseEngineerTaskMangerApplication.java
@@ -0,0 +1,13 @@
+package ReverseEngineerTaskManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseEngineerTaskMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseEngineerTaskMangerApplication.class, args);
+	}
+
+}
