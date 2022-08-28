59227d1062cc43ffce092ee7366781c5c605c1cc
Yaseen asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2022-08-28 19:10:53 +0300

    Update pom

---
+++ controller/src/test/java/com.exalt.springboot/unittest/controllers/tt.java
@@ -0,0 +1,12 @@
+package com.exalt.springboot.unittest.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class tt {
+    @Test
+    void ss() {
+        System.out.println(1);
+    }
+}
